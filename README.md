d4a324bd4fe724ccce5c6a27f25e4f35d711cfe6
juan-bolivar <j_bolivar@outlook.com>
2022-08-23 19:30:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# INSTRUCTIONS
+
+## Configuration
+
+Create a file *credentials.env* with the following variables:
+
+
+AWS_ACCESS_KEY_ID=
+
+AWS_SECRET_ACCESS_KEY=
+
+AWS_REGION= 
+
+AWS_DEFAULT_REGION= 
+
+DATA_DOG_API_KEY=
+
+## Run
+
+
+Please run with the following command
+
+
+```
+make
+
+```
+
+then open the concourse web UI *localhost:8080* and enter:
+
+```
+user: test
+password: test
+```
+
---
+++ datadog/variables.tf
@@ -0,0 +1,25 @@
+variable "datadog_api_key" {
+  type = string
+  description = "Datadog API Key"
+}
+
+variable "datadog_app_key" {
+  type = string
+  description = "Datadog API Key"
+}
+
+
+variable "cluster_name" {
+  type = string
+  description = "Cluster Name"
+}
+
+variable "cluster_endpoint" {
+  type = string
+  description = "Cluster Endpoint"
+}
+
+variable "cluster_ca_cert" {
+  type = string
+}
+  
