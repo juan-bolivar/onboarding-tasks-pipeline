654fbfcbc1b412a3f3826f640271b49509ee0b45
juan-bolivar <j_bolivar@outlook.com>
2022-08-25 16:13:51 -0500

    Fixed some fluxcd script error

--- fluxcd/script.sh
+++ fluxcd/script.sh
@@ -3,24 +3,12 @@
 
 aws eks update-kubeconfig --name my-cluster
 
-flux bootstrap github \
-	--owner=$GITHUB_USER\
-	--repository=onboarding-tasks-pipeline\
-	--branch=fluxcd\
-	--path=app-cluster\
-	--personal
+flux bootstrap github --owner=$GITHUB_USER --repository=onboarding-tasks-pipeline --branch=fluxcd --path=app-cluster --personal
 
-flux create source helm helm-chart --url https://juan-bolivar.github.io/onboarding-tasks-pipeline \ 
-	--interval 1m0s\
-	--export > helmrepo.yaml
+flux create source helm helm-chart --url https://juan-bolivar.github.io/onboarding-tasks-pipeline --interval 1m0s --export > helmrepo.yaml
 
 kubectl apply -f helmrepo.yaml --cluster=$(aws eks describe-cluster --name my-cluster | jq -r .cluster.arn)
 
-flux create helmrelease helm-chart \
-	--source=HelmRepository/helm-chart\
-	--chart helm-chart\
-	--target-namespace default \
-	--interval 3m0s \
-	--export > helmrelease.yaml
+flux create helmrelease helm-chart --source=HelmRepository/helm-chart --chart helm-chart --target-namespace default --interval 3m0s  --export > helmrelease.yaml
 
 kubectl apply -f helmrelease.yaml --cluster=$(aws eks describe-cluster --name my-cluster | jq -r .cluster.arn)
